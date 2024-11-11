1b4def6f6b27d08696a998a554cfa70ca4bf0082
tanert48 <iake1154@gmail.com>
2024-11-11 08:40:01 +0000

    add testing case

--- test_zoo.py
+++ test_zoo.py
@@ -9,6 +9,46 @@ class TestZoo(unittest.TestCase):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
     # Add your additional test cases here.
+    #ECP
+    def test_1_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_2_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_3_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)    
+
+    def test_4_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), 'invalid')
+    
+    def test_5_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+
+    def test_6_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+    #BVA
+    def test_7_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), 'invalid')
+    def test_8_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+    def test_9_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+    def test_10_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+    def test_11_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+    def test_12_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+    def test_13_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+    def test_14_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+    def test_15_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(90), 100)
+    def test_16_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(91), 100)
+    
 
 if __name__ == '__main__':
     unittest.main()
